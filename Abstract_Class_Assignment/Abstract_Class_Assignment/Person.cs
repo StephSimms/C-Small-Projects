a91d160f35b7b781e829ea1e3fe42898baae9bb5
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-05-30 15:42:43 -0300

    Abstract Assignment

---
+++ Abstract_Class_Assignment/Abstract_Class_Assignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Class_Assignment
+{
+    class Person
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public void SayName()
+        {
+
+        }
+    }
+}
---
+++ Abstract_Class_Assignment/Abstract_Class_Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.fName = "Sample";
+            person.lName = "Sample Sample";
+
+            person.SayName(person.fName, person.lName);
+
+            Console.ReadLine();
+
+        }
+    }
+}
