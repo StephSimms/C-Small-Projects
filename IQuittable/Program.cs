9231f4ca2c90cada047e7f788f5130e949dddbee
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-07 13:52:06 -0300

    test

---
+++ IQuittable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Interface_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.fName = "Sample";
+            employee.lName = "Student";
+
+            employee.SayName(employee.fName, employee.lName);
+
+            Console.ReadLine();
+        }
+    }
+}
