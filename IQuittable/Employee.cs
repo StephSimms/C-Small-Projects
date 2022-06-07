c4080989fdbd119842bc4855d6b6133b87271045
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-07 13:53:17 -0300

    test

---
+++ IQuittable/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface_Assignment
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+        public void SayName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + " " + lastName);
+
+        }
+    }
+}
